d740f2fc5834d661d54f8f4ffa471d818a8786d8
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 22:51:30 +0800

    feat: Add ReviewRequestDto for submitting product reviews with rating and comment

---
+++ Ecommerce.Shared/Reviews/ReviewRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.Reviews
+{
+    public record ReviewRequestDto
+    {
+         public Guid Id { get; init; }
+         public int Rating { get; init; } // 1-5
+         public string Comment { get; init; } = string.Empty;
+    }
+}
