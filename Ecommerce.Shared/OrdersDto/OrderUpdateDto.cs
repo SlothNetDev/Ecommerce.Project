a906e041d55aca7da6b296773382f36f7fdcb2ce
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 21:10:36 +0800

    feat: Add OrderUpdateDto for order update data transfer with properties for Id, Status, and TotalAmount

---
+++ Ecommerce.Shared/OrdersDto/OrderUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.OrdersDto
+{
+    public class OrderUpdateDto
+    {
+        public Guid Id { get; init; }
+        public Status? Status { get; init; } // Pending, Confirmed, Shipped, Delivered, Cancelled
+        public decimal? TotalAmount { get; init; }
+    }
+}
