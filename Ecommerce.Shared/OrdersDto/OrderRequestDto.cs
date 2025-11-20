bb2a90a8df428e572642e7806b13b6b1524c5266
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 21:10:26 +0800

    feat: Add OrderRequestDto for order data transfer with properties for Id, Status, and TotalAmount

---
+++ Ecommerce.Shared/OrdersDto/OrderRequestDto.cs
@@ -0,0 +1,12 @@
+﻿
+using Ecommerce.Shared.Entities;
+
+namespace Ecommerce.Shared.OrdersDto
+{
+    public record OrderRequestDto
+    {
+         public Guid Id { get; init; }
+         public Status? Status { get; init; } // Pending, Confirmed, Shipped, Delivered, Cancelled
+         public decimal TotalAmount { get; init; }
+    }
+}
