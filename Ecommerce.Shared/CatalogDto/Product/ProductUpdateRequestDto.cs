d402e24c038f216ffffdf612224f3aab4fc7aa93
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 19:50:03 +0800

    feat: Add ProductUpdateRequestDto for updating product details including image upload support

---
+++ Ecommerce.Shared/CatalogDto/Product/ProductUpdateRequestDto.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Shared.CatalogDto.Product
+{
+    public class ProductUpdateRequestDto
+    {
+         public Guid Id { get; set; }
+         public string? Name { get; set; } = string.Empty;
+         public string? Description { get; set; }
+         public decimal Price { get; set; }
+         public int StockQuantity { get; set; }
+         public IFormFile? ImageUrl { get; set; }
+    }
+}
