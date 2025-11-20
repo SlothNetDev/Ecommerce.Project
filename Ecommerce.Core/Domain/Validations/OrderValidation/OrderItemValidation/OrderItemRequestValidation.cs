0c92b94ba278f5642eabf773bfe3db8614162ea6
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 22:44:19 +0800

    feat: Implement OrderItemRequestValidation to define validation rules for OrderItemRequestDto

---
+++ Ecommerce.Core/Domain/Validations/OrderValidation/OrderItemValidation/OrderItemRequestValidation.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.Shared.OrdersDto.OrderItemDto;
+using FluentValidation;
+
+
+namespace Ecommerce.Core.Domain.Validations.OrderValidation.OrderItem
+{
+    internal class OrderItemRequestValidation : AbstractValidator<OrderItemRequestDto>
+    {
+    }
+}
