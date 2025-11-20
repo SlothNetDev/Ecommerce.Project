11a8fb2f2940dca6a4e59ccc3657cd3383fe8e48
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 20:13:58 +0800

    feat: Add ProductResponseDto for product response data transfer with properties for Id, Name, Description, Price, StockQuantity, and ImageUrl

---
+++ Ecommerce.Shared/CatalogDto/Product/ProductResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.CatalogDto.Product
+{
+    public record ProductResponseDto
+    {
+         public Guid Id { get; init; }
+         public string? Name { get; init; } = string.Empty;
+         public string? Description { get;  init; }
+         public decimal Price { get; init; }
+         public int StockQuantity { get; init; }
+         public string? ImageUrl { get; init; }
+    }
+}
