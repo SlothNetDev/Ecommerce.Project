40f95b79e4e43ca42a2cb6ee619784e52ba7b145
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 23:31:15 +0800

    feat: Implement ReviewRequestValidation to enforce validation rules for review submissions

---
+++ Ecommerce.Core/Domain/Validations/Reviews/ReviewRequestValidation.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Shared.Reviews;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Domain.Validations.Reviews
+{
+    internal class ReviewRequestValidation : AbstractValidator<ReviewRequestDto>
+    {
+        protected ReviewRequestValidation()
+        {
+            RuleFor(x => x.Rating)
+                .InclusiveBetween(1, 5).WithMessage("Rating must be between 1 and 5.");
+
+            RuleFor(x => x.Comment)
+                .MaximumLength(1000).WithMessage("Comment must not exceed 1000 characters.");
+        }
+    }
+}
