f414ba645e467ae4e69543696df53a5bbd315609
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 19:47:02 +0800

    feat: Add ProductRequestDto for product data transfer and include Microsoft.AspNetCore.Http.Abstractions package

---
+++ Ecommerce.Shared/CatalogDto/Product/ProductRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Shared.CatalogDto.Product
+{
+    public class ProductRequestDto
+    {
+         public string Name { get; set; } = string.Empty;
+         public string? Description { get; set; }
+         public decimal Price { get; set; }
+         public int StockQuantity { get; set; }
+         public IFormFile? Image { get; set; } 
+    }
+}
