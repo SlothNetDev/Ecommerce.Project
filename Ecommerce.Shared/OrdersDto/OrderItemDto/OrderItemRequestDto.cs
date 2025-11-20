25cf9a574b9064cb72ae14c2eee41a03cef85ac3
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 21:41:18 +0800

    feat: Add OrderItemRequestDto for order item request data transfer with properties for Quantity and UnitPrice

---
+++ Ecommerce.Shared/OrdersDto/OrderItemDto/OrderItemRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.OrdersDto.OrderItemDto
+{
+    public record OrderItemRequestDto
+    {
+        public int Quantity { get; init; }
+        public decimal UnitPrice { get; init; }
+    }
+}
