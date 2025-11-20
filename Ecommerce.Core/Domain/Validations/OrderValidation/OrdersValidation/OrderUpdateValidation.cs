93928d49e05cba5b085666db84d606894beb806b
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 21:10:00 +0800

    feat: Implement OrderUpdateValidation for validating order updates with rules for order status and total amount constraints

---
+++ Ecommerce.Core/Domain/Validations/OrderValidation/OrdersValidation/OrderUpdateValidation.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Shared.OrdersDto;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Domain.Validations.OrderValidation.OrdersValidation
+{
+    internal class OrderUpdateValidation :AbstractValidator<OrderUpdateDto>
+    {
+        protected OrderUpdateValidation()
+        {
+            RuleFor(status => status.Status)
+                .IsInEnum().WithMessage("Invalid order status.");
+
+            // --- Rule for Price ---
+            RuleFor(product => product.TotalAmount)
+                .GreaterThan(0)
+                .WithMessage("Total AmountTotal Amount must be greater than zero.")
+                .LessThanOrEqualTo(1_000_000_000)
+                .WithMessage("Total Amount cannot exceed 10 digits");
+
+
+
+
+        }
+    }
+}
