8ed97ab111c56c21340fb6ff1e75e7a6d4ea8036
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 22:53:52 +0800

    chore: Remove Id property from ReviewRequestDto as it is no longer needed

--- Ecommerce.Shared/Reviews/ReviewRequestDto.cs
+++ Ecommerce.Shared/Reviews/ReviewRequestDto.cs
@@ -8,7 +8,6 @@ namespace Ecommerce.Shared.Reviews
 {
     public record ReviewRequestDto
     {
-         public Guid Id { get; init; }
          public int Rating { get; init; } // 1-5
          public string Comment { get; init; } = string.Empty;
     }
