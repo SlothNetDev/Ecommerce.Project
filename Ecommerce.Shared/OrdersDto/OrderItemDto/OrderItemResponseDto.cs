cca3bb1295832594f7d1619dc882ee1cdfac4301
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 21:41:07 +0800

    feat: Add OrderItemResponseDto for order item response data transfer with properties for Id, Quantity, and UnitPrice

---
+++ Ecommerce.Shared/OrdersDto/OrderItemDto/OrderItemResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.OrdersDto.OrderItemDto
+{
+    public record OrderItemResponseDto
+    {
+        public Guid Id { get; init; }
+        public int Quantity { get; init; }
+        public decimal UnitPrice { get; init; }
+    }
+}
