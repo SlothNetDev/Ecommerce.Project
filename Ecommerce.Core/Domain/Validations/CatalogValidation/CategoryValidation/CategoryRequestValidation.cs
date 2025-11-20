f6a8976e261eb252c6b7520a9b24a58fcde14f6d
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 09:29:27 +0800

    feat: Implement CategoryRequestValidation for category DTO validation

---
+++ Ecommerce.Core/Domain/Validations/CatalogValidation/CategoryValidation/CategoryRequestValidation.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Shared.CatalogDto.Category;
+using FluentValidation;
+
+
+namespace Ecommerce.Core.Domain.Validations.CatalogValidation.CategoryValidation
+{
+    internal class CategoryRequestValidation : AbstractValidator<CategoryRequestDto>
+    {
+        protected CategoryRequestValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .MaximumLength(100).WithMessage("Category name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Category description must not exceed 500 characters.");
+        }
+
+    }
+}
