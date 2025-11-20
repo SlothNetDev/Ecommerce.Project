a0ccdc8bb141913ff8580490f9cd354b8d895744
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 08:09:45 +0800

    Feat: Adding OrderItem entities

---
+++ Ecommerce.Core/Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Core.Domain.Entities.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Domain.Entities.Orders
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; } // Snapshot at time of order
+        
+        // Navigation
+        public virtual Order Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
