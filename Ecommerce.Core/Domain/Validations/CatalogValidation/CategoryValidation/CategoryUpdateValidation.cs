76e382e03e6b322d2d110d5a08b91c467465580b
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 09:29:43 +0800

    feat: Add CategoryUpdateValidation for category update request validation

---
+++ Ecommerce.Core/Domain/Validations/CatalogValidation/CategoryValidation/CategoryUpdateValidation.cs
@@ -0,0 +1,27 @@
+﻿using Ecommerce.Shared.CatalogDto.Category;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Domain.Validations.CatalogValidation.CategoryValidation
+{
+    internal class CategoryUpdateValidation : AbstractValidator<CategoryUpdateRequestDto>
+    {
+        protected CategoryUpdateValidation()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Category ID is required.")
+                .Must(id => id != Guid.Empty).WithMessage("Category ID must be a valid GUID.");
+
+            RuleFor(x => x.Name)
+                .MaximumLength(100).WithMessage("Category name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Category description must not exceed 500 characters.");
+
+        }
+    }
+}
