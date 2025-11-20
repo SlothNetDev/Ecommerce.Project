15641a7997e108a55f80abfe7c9e21b9ca4dbd37
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 09:30:08 +0800

    feat: Add CategoryUpdateRequestDto for category update data transfer

---
+++ Ecommerce.Shared/CatalogDto/Category/CategoryUpdateRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.CatalogDto.Category
+{
+    public record CategoryUpdateRequestDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string? Description { get; init; }
+    }
+}
