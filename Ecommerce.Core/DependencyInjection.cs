e4a72ef4ac799a83895c4c62e42b7b8900fb56e2
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 09:29:10 +0800

    feat: Add DependencyInjection class for service registration in Infrastructure layer

---
+++ Ecommerce.Core/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Core.Domain.Validations.CatalogValidation.CategoryValidation;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Core
+{
+    /// <summary>
+    /// Creates extension methods for registering services in the Infrastructure layer.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            #region
+            //Category Validation
+            services.AddValidatorsFromAssemblyContaining<CategoryRequestValidation>();
+            services.AddValidatorsFromAssemblyContaining<CategoryUpdateValidation>();
+            #endregion
+
+            return services;
+        }
+    }
+}
