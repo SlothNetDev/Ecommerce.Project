b1038e3cd4bbffc24093661465659c461045abfc
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 23:32:30 +0800

    feat: Add StatusUpdateBySellerDto for order item status updates by sellers

---
+++ Ecommerce.Shared/OrdersDto/OrderItemDto/StatusUpdateBySellerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.OrdersDto.OrderItemDto
+{
+    public record StatusUpdateBySellerDto
+    {
+        public Guid OrderItemId { get; init; } // Unique identifier for the order item
+        public string Status { get; init; } = null!; // e.g., Shipped, Delivered
+    }
+}
