c7d21b519d1ce32b24d27d3d1f516dfb75174982
ProgrammerNoob <sungshigun@gmail.com>
2025-11-21 00:17:22 +0800

    feat: Update Category entity to include Seller and ParentCategory properties for enhanced category management

--- Ecommerce.Core/Domain/Entities/Catalog/Category.cs
+++ Ecommerce.Core/Domain/Entities/Catalog/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ecommerce.Core.Domain.Entities.UserManagement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,23 @@ namespace Ecommerce.Core.Domain.Entities.Catalog
 {
     public class Category
     {
-        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
-        
+
+        //For admin or seller categories
+        public Guid? SellerId { get; set; } // Null if created by Admin
+        public User? Seller { get; set; }
+
+
+        // Self-referencing for subcategories
+        public Guid? ParentCategoryId { get; set; } 
+        public Category? ParentCategory { get; set; }
+
+        public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+
+
+        // Products in this category
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
