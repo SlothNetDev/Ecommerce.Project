928c3799e1a35acad9994f31367d425686f01b50
ProgrammerNoob <sungshigun@gmail.com>
2025-11-20 08:10:00 +0800

    feat: Adding Review entities

---
+++ Ecommerce.Core/Domain/Entities/Reviews/Review.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Core.Domain.Entities.Catalog;
+using Ecommerce.Core.Domain.Entities.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Domain.Entities.Reviews
+{
+    public class Review
+    {
+         public Guid Id { get; set; }
+         public Guid ProductId { get; set; }
+         public Guid UserId { get; set; }
+         public int Rating { get; set; } // 1-5
+         public string Comment { get; set; } = string.Empty;
+         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+         
+         // Navigation
+         public virtual Product Product { get; set; } = null!;
+         public virtual User User { get; set; } = null!;
+    }
+}
