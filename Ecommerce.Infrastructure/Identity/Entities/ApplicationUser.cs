34743042f17c1494274f227c109566145a3e1e7e
ProgrammerNoob <sungshigun@gmail.com>
2025-11-22 19:36:36 +0800

    feat: add ApplicationUser entity for identity management with extended properties and domain user linkage

---
+++ Ecommerce.Infrastructure/Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Core.Domain.Entities.UserManagement;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Infrastructure.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+        public DateTime? AccountCreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? AccountUpdatedAt { get; set; }
+        public DateTime? AccountDeletedAt { get; set; }
+        
+        
+        //link to domain user entities "User"
+        public Guid DomainUserId { get; set; }
+        public User DomainUser { get; set; } = null!;
+
+        //Add refresh token to track all active/Inactive refresh tokens
+      /*  public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();*/
+    }
+}
