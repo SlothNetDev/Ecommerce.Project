8c85d1b7ed9b8d9d53fe91e90b75dd6e2b1dcfa4
ProgrammerNoob <sungshigun@gmail.com>
2025-11-22 19:36:20 +0800

    feat: Add custom ApplicationUser and ApplicationRole entities for extended identity management.

---
+++ Ecommerce.Infrastructure/Identity/Entities/ApplicationRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Identity.Entities
+{
+    internal class ApplicationRole : IdentityRole<Guid>
+    {
+        //provide roles
+        public string Description { get; set; } = string.Empty;
+
+        //used for time stamp
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
