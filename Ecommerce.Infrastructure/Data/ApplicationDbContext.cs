56de38ac6b72fbabc7ebc001161c32dcf00ffddd
ProgrammerNoob <sungshigun@gmail.com>
2025-11-22 19:33:03 +0800

    feat: Implement ApplicationDbContext for Entity Framework Core integration with Identity framework

---
+++ Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Ecommerce.Infrastructure.Data
+{
+    internal class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser,ApplicationRole, Guid>(options)     
+    { 
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
