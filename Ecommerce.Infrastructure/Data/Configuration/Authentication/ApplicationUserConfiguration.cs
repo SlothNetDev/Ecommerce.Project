8e0db7dfddcf2e6aad03a3e2c5925b31294be8b6
ProgrammerNoob <sungshigun@gmail.com>
2025-11-22 19:36:09 +0800

    feat: Introduce `ApplicationUser` and `ApplicationRole` entities for identity management and configure `ApplicationUser` with EF Core.

---
+++ Ecommerce.Infrastructure/Data/Configuration/Authentication/ApplicationUserConfiguration.cs
@@ -0,0 +1,32 @@
+using Ecommerce.Core.Domain.Entities.UserManagement;
+using Ecommerce.Infrastructure.Identity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Authentication
+{
+    internal class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.ToTable("ApplicationUsers");
+
+            builder.Property(u => u.FirstName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.LastName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.IsActive)
+                .HasDefaultValue(true);
+
+            // Configure relationship with Domain User (1:1)
+            builder.HasOne(u => u.DomainUser)
+                .WithOne()
+                .HasForeignKey<ApplicationUser>(u => u.DomainUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
