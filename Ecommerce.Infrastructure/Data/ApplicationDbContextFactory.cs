5324c0fc82043ee31bf0ca28186f63ec66eb26bc
ProgrammerNoob <sungshigun@gmail.com>
2025-11-22 19:33:20 +0800

    feat: Add ApplicationDbContextFactory for design-time database context creation

---
+++ Ecommerce.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Ecommerce.Infrastructure.Data
+{
+    internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // now works
+            .AddUserSecrets<ApplicationDbContextFactory>() // for development
+            .Build();
+
+            var connectionString = config.GetConnectionString("EcommerceDbConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Could not find connection string 'EcommerceDbConnection'");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
