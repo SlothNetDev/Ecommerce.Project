8b01220dc87b2d6168c28964185b1438ff4b6cde
ProgrammerNoob <sungshigun@gmail.com>
2025-11-23 09:23:53 +0800

    feat: Initialize database context with Identity and core entity configurations.

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,25 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ecommerce.Core;
+using Ecommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Ecommerce.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //use the validation
+            builder.Services.AddInfrastructure();
+
+            //Add sql
+            builder.Services.AddDbContext<ApplicationDbContext>(temp =>
+            {
+                temp.UseSqlServer(builder
+                    .Configuration.GetConnectionString("EcommerceDbConnection"));
+            });
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+            app.UseHttpsRedirection();
+            
+            app.UseAuthorization();
+            
+            app.MapControllers();
+            
+            app.Run();
+
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
