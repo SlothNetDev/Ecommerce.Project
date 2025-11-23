0f45a2a8eaa306ba004f5e318123429e64672b89
ProgrammerNoob <sungshigun@gmail.com>
2025-11-23 09:32:53 +0800

    feat: Add EF Core entity configurations for catalog, orders, and reviews, and introduce initial Identity entities and migrations.

---
+++ Ecommerce.Infrastructure/Data/Configuration/Catalog/CategoryConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Ecommerce.Core.Domain.Entities.Catalog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Catalog
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(c => c.CategoryId);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(500);
+
+            //relationships to seller and parent category
+            builder.HasOne(c => c.Seller)
+                .WithMany(x => x.Categories)
+                .HasForeignKey(c => c.SellerId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //relationship for parent category
+            builder.HasOne(c => c.ParentCategory)
+                .WithMany(c => c.SubCategories)
+                .HasForeignKey(c => c.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
