522b55fad04a3751068baf73773ba344bf9e504f
ProgrammerNoob <sungshigun@gmail.com>
2025-11-23 09:24:30 +0800

    feat: Add initial EF Core entity configurations and migrations for e-commerce domain models and identity roles.

--- Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
+++ Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,10 @@
-﻿using Ecommerce.Infrastructure.Identity.Entities;
+﻿using Ecommerce.Core.Domain.Entities.Catalog;
+using Ecommerce.Core.Domain.Entities.Orders;
+using Ecommerce.Core.Domain.Entities.Reviews;
+using Ecommerce.Infrastructure.Data.Configuration.Catalog;
+using Ecommerce.Infrastructure.Data.Configuration.NewFolder;
+using Ecommerce.Infrastructure.Data.Configuration.Orders;
+using Ecommerce.Infrastructure.Identity.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,14 +12,25 @@ using Microsoft.EntityFrameworkCore;
 namespace Ecommerce.Infrastructure.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options)
-    { 
-        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
-        
+    {
+        public virtual DbSet<ApplicationUser> ApplicationUsersDb { get; set; } = null!;
+        public virtual DbSet<Category> CategoriesDb { get; set; } = null!;
+        public virtual DbSet<Product> ApplicationRolesDb { get; set; } = null!;
+        public virtual DbSet<Order> OrderDb { get; set; } = null!;
+        public virtual DbSet<OrderItem> OrderItemDb { get; set; } = null!;
+        public virtual DbSet<Review> ReviewDb { get; set; } = null!;
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             
             builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
+            builder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+            builder.ApplyConfigurationsFromAssembly(typeof(OrderItemConfiguration).Assembly);
+            builder.ApplyConfigurationsFromAssembly(typeof(OrderConfiguration).Assembly);
+            builder.ApplyConfigurationsFromAssembly(typeof(ReviewConfiguration).Assembly);
         }
     }
 }
