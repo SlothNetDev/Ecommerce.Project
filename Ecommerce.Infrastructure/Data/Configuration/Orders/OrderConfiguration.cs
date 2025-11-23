c573cd8c0c963509602224035a62640c0ced7ab9
ProgrammerNoob <sungshigun@gmail.com>
2025-11-23 09:34:18 +0800

    feat: Add Identity user and role entities and configure Order and OrderItem entities.

---
+++ Ecommerce.Infrastructure/Data/Configuration/Orders/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Core.Domain.Entities.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Orders
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Order Table",
+                t => t.HasCheckConstraint("CK_YourEntity_Status", 
+                "[Status] IN ('Pending', 'Confirmed', 'Shipped', 'Delivered', 'Cancelled')"));
+
+            builder.Property(x => x.Status)
+                .HasMaxLength(20);
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.TotalAmount)
+                .HasPrecision(18, 2)
+                .IsRequired();
+        }
+    }
+}
