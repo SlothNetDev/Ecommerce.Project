b72aecdae102604452802dd9ea67f84f94d7ea58
slothnetdev <sungshigun@gmail.com>
2025-11-25 06:07:21 +0800

    feat: Implement token-based authentication and refresh token management, refactor review DTOs, and update OrderResponseDto visibility.

---
+++ Ecommerce.Core/Application/Common/Interfaces/IRefreshTokenService.cs
@@ -0,0 +1,27 @@
+using Ecommerce.Shared.AuthenticationDTO;
+using Ecommerce.Shared.TokenDTO;
+using Ecommerce.Shared.Wrapper;
+
+namespace Ecommerce.Core.Application.Common.Interfaces;
+
+public interface IRefreshTokenService
+{
+    /// <summary>
+    /// Generating Refresh Token for an Account
+    /// </summary>
+    /// <param name="token"></param>
+    /// <param name="refreshToken"></param>
+    /// <returns>AuthResponseDto </returns>
+    Task<ResponseType<RefreshTokenResponseDto>> RefreshTokenAsync(string token, string refreshToken);
+    /// <summary>
+    /// Get all RefreshToken of Accounts
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns>List of Account RefreshToken</returns>
+    Task<ResponseType<List<RefreshTokenResponseDto>>> GetRefreshTokenAsync(Guid userId);
+
+    /// <summary>
+    /// Logs the user out by revoking the refresh token.
+    /// </summary>
+    Task<ResponseType<string>> LogoutAsync(LogOutRequestDto dto);
+}
