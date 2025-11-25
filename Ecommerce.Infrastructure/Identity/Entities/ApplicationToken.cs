36b3cbf266c97d48955f204c974859664e80115a
slothnetdev <sungshigun@gmail.com>
2025-11-25 08:03:53 +0800

    feat: Add new test project and introduce `ApplicationToken` entity, modifying `ApplicationUser`.

---
+++ Ecommerce.Infrastructure/Identity/Entities/ApplicationToken.cs
@@ -0,0 +1,23 @@
+namespace Ecommerce.Infrastructure.Identity.Entities;
+
+public class ApplicationToken
+{
+    public string Id { get; set; } // Primary key
+    public string Token { get; set; } = string.Empty; // Actual refresh token string
+    public DateTime Expires { get; set; } // Expiration date
+    public bool IsExpired => DateTime.UtcNow >= Expires; //expression embodied property
+         
+    public DateTime Created { get; set; }
+    public string CreatedByIp { get; set; } = string.Empty;
+         
+    public DateTime? Revoked { get; set; }
+    public string? RevokedByIp { get; set; }
+         
+    public bool IsActive => Revoked == null && !IsExpired; //returns true if both was true return true
+    
+    // Add reason for revocation
+    public string? RevocationReason { get; set; } // "UserLogout", "SuspiciousActivity", etc.
+    // Navigation
+    public Guid UserId { get; set; }
+    public ApplicationUser User { get; set; } = null!;
+}
