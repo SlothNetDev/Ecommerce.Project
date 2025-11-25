bac9785a6f78a23838050e58235140b451e9fec4
slothnetdev <sungshigun@gmail.com>
2025-11-25 08:03:35 +0800

    feat: add new `Ecommerce.Test` project, `ApplicationToken` entity, and update `ITokenService` and `ApplicationUser`.

--- Ecommerce.Core/Application/Common/Interfaces/ITokenService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/ITokenService.cs
@@ -7,11 +7,17 @@ namespace Ecommerce.Core.Application.Common.Interfaces;
 public interface ITokenService
 {
     /// <summary>
-    /// Generating Token
+    /// Generating Token for Users
     /// </summary>
     /// <param name="user"></param>
     /// <returns></returns>
     Task<ResponseType<AuthenticationResponseDto>> GenerateTokenAsync(TokenUserDto user); 
     
+    /// <summary>
+    ///  Generating the refreshtoken that will use to extend the login period or token that will be revoked
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="ipAddress"></param>
+    /// <returns></returns>
     ResponseType<RefreshTokenResponseDto> GenerateRefreshToken(string userId, string ipAddress);
 }
