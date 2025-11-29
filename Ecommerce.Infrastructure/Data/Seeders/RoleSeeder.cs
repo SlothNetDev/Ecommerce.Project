d358f683b174604072f3115e63e20b67787894cf
slothnetdev <sungshigun@gmail.com>
2025-11-29 08:49:48 +0800

    feat: Establish integration test framework for authentication and authorization with dedicated utilities and initial test cases.

---
+++ Ecommerce.Infrastructure/Data/Seeders/RoleSeeder.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.Infrastructure.Data.Seeders;
+
+//Roles available 
+public class RoleSeeder
+{
+    public const string Admin = "Admin";
+    public const string Costumer = "Costumer";
+    public const string Seller = "Seller";
+}
