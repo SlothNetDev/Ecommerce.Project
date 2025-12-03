3953cf0802a09c91fc498a1e431bf492275360d3
slothnetdev <sungshigun@gmail.com>
2025-12-03 10:53:48 +0800

    feat: Implement refresh token functionality, new authentication services, and related tests.

--- Ecommerce.Core/Application/Common/Interfaces/ITokenService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/ITokenService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Ecommerce.Shared.AuthenticationDTO;
 using Ecommerce.Shared.TokenDTO;
 using Ecommerce.Shared.Wrapper;
@@ -6,18 +7,6 @@ namespace Ecommerce.Core.Application.Common.Interfaces;
 
 public interface ITokenService
 {
-    /// <summary>
-    /// Generating Token for Users
-    /// </summary>
-    /// <param name="user"></param>
-    /// <returns></returns>
-    Task<ResponseType<AuthenticationResponseDto>> GenerateTokenAsync(TokenUserDto user); 
-    
-    /// <summary>
-    ///  Generating the refreshtoken that will use to extend the login period or token that will be revoked
-    /// </summary>
-    /// <param name="userId"></param>
-    /// <param name="ipAddress"></param>
-    /// <returns></returns>
-    ResponseType<RefreshTokenResponseDto> GenerateRefreshToken(string userId, string ipAddress);
+    string CreateJwtToken(IEnumerable<Claim> claims);
+    IEnumerable<Claim> BuildClaims(TokenUserDto user);
 }
