e76d87e433a9e8303c8e54dae5d6539103d453c2
slothnetdev <sungshigun@gmail.com>
2025-12-03 20:48:00 +0800

    feat: configure Review entity, rename ApplicationToken primary key, and redefine RefreshTokenDto structure.

---
+++ Ecommerce.Infrastructure/Data/Configuration/Review/ReviewConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Review
+{
+    internal class ReviewConfiguration : IEntityTypeConfiguration<Core.Domain.Entities.Reviews.Review>
+    {
+        public void Configure(EntityTypeBuilder<Core.Domain.Entities.Reviews.Review> builder)
+        {
+            builder.ToTable("Reviews");
+
+            builder.HasKey(r => r.Id);
+
+            builder.ToTable("Review",t => 
+            t.HasCheckConstraint("CK_Reviews_Rating","[Rating] >= 1 AND [Rating] <= 5"));
+
+
+            builder.Property(r => r.Comment)
+                   .HasMaxLength(1000);
+        }
+    }
+}
