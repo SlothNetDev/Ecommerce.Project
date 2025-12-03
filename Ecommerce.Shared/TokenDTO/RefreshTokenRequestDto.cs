9cf0049427add79452f73aabb8879d69be8419c3
slothnetdev <sungshigun@gmail.com>
2025-12-03 20:51:04 +0800

    feat: add comprehensive token lifecycle and management properties to RefreshTokenDto.

--- Ecommerce.Shared/TokenDTO/RefreshTokenRequestDto.cs
+++ Ecommerce.Shared/TokenDTO/RefreshTokenRequestDto.cs
@@ -3,7 +3,21 @@ namespace Ecommerce.Shared.TokenDTO;
 /// <summary>
 /// Create refresh token request
 /// </summary>
-public record RefreshTokenDto(
-    string BearerToken,
-    string RefreshToken
-);
+public record RefreshTokenDto
+{
+    public string TokenId { get; set; } // Primary key
+    public string Token { get; set; } = string.Empty; // Actual refresh token string
+    public DateTime Expires { get; set; } // Expiration date
+    public bool IsExpired => DateTime.UtcNow >= Expires; //expression embodied property
+         
+    public DateTime Created { get; set; }
+    public string CreatedByIp { get; set; } = string.Empty;
+         
+    public DateTime? Revoked { get; set; }
+    public string? RevokedByIp { get; set; }
+         
+    public bool IsActive => Revoked == null && !IsExpired; //returns true if both was true return true
+    
+    // Add reason for revocation
+    public string? RevocationReason { get; set; } // "UserLogout", "SuspiciousActivity", etc.
+}
