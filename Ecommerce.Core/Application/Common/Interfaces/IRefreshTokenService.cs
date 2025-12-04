86d9a3caf45df115b7eaf02df859aa801ac14099
slothnetdev <sungshigun@gmail.com>
2025-12-04 09:06:54 +0800

    refactor: update refresh token service to use `RefreshTokenResponseDto` for saving and `string` for token validation.

--- Ecommerce.Core/Application/Common/Interfaces/IRefreshTokenService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/IRefreshTokenService.cs
@@ -8,10 +8,10 @@ namespace Ecommerce.Core.Application.Common.Interfaces;
 public interface IRefreshTokenService
 {
     RefreshTokenResponseDto GenerateRefreshToken(string userId, string ipAddress);
-    Task SaveRefreshTokenAsync(string refreshToken);
+    Task SaveRefreshTokenAsync(RefreshTokenResponseDto refreshToken);
     Task<ResponseType<RefreshTokenResponseDto>> GetStoredTokenAsync(string refreshToken);
     Task<string> RevokeTokenAsync(string refreshToken, string ipAddress, string reason, string? replacedByToken = null);
     Task<ResponseType<RefreshTokenResponseDto>> RotateTokenAsync(string refreshToken, string ipAddress, string revocationReason);
-    Task<bool> IsTokenValidAsync(ApplicationTokenDto token);
+    Task<bool> IsTokenValidAsync(string token);
 
 }
