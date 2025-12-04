265fe7fdc5e7fb614118550ab2ec9255c151fb4e
slothnetdev <sungshigun@gmail.com>
2025-12-05 07:48:32 +0800

    feat: Add refresh token endpoint to authentication controller and update refresh token service to return async `ResponseType`.

--- Ecommerce.Core/Application/Common/Interfaces/IRefreshTokenService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/IRefreshTokenService.cs
@@ -7,7 +7,7 @@ namespace Ecommerce.Core.Application.Common.Interfaces;
 
 public interface IRefreshTokenService
 {
-    RefreshTokenResponseDto GenerateRefreshToken(string userId, string ipAddress);
+    Task<ResponseType<RefreshTokenResponseDto>> GenerateRefreshToken(string userId, string ipAddress);
     Task SaveRefreshTokenAsync(RefreshTokenResponseDto refreshToken);
     Task<ResponseType<RefreshTokenResponseDto>> GetStoredTokenAsync(string refreshToken);
     Task<string> RevokeTokenAsync(string refreshToken, string ipAddress, string reason, string? replacedByToken = null);
