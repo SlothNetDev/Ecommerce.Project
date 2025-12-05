26adec48913b5d2bdf4870a1c43476436c0a96cc
slothnetdev <sungshigun@gmail.com>
2025-12-05 17:15:49 +0800

    feat: Add RefreshToken endpoint to AuthenticationController for token renewal functionality

--- Ecommerce.Api/Controllers/AuthenticationController.cs
+++ Ecommerce.Api/Controllers/AuthenticationController.cs
@@ -1,5 +1,8 @@
+using Ecommerce.Core.Application.Common.Interfaces;
+using Ecommerce.Core.Application.Common.Interfaces.RefreshToken;
 using Ecommerce.Infrastructure.Identity.Entities;
 using Ecommerce.Shared.AuthenticationDTO;
+using Ecommerce.Shared.TokenDTO;
 using Ecommerce.Shared.Wrapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ecommerce.Api.Controllers;
 
 [ApiController]
-[Route("api/auth")]
+[Route("api/auth/action")]
 public class AuthenticationController(
-    UserManager<ApplicationUser> userManager) : ControllerBase
+    UserManager<ApplicationUser> userManager,
+    ITokenRefreshService refreshTokenService) : ControllerBase
 {
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequestDto request)
@@ -39,5 +43,24 @@ public class AuthenticationController(
             "Authentication Successful"));
     }
     
-    
+    [HttpPost("RefreshToken")]
+    [ProducesResponseType(typeof(TokenResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> RefreshToken([FromBody]RefreshTokenRequestDto  request )
+    {
+        var response = await refreshTokenService.RefreshTokenAsync(request);
+
+        if (!response.Success)
+        {
+            return BadRequest(new ProblemDetails
+            {
+                Title = "Token Refresh Failed",
+                Detail = response.Message,
+                Status = StatusCodes.Status400BadRequest
+            });
+        }
+
+        return Ok(response);
+    }
 }
