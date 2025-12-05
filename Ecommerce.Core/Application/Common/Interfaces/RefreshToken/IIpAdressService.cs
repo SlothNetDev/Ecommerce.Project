c5610d7fa1e904b797365a893a097ae69c0b9b96
slothnetdev <sungshigun@gmail.com>
2025-12-05 12:31:52 +0800

    feat: Implement refresh token endpoint with IP address service integration and update authentication tests.

--- Ecommerce.Core/Application/Common/Interfaces/RefreshToken/IIpAdressService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/RefreshToken/IIpAdressService.cs
@@ -2,5 +2,7 @@ namespace Ecommerce.Core.Application.Common.Interfaces.RefreshToken;
 
 public interface IIpAdressService
 {
-    
+    string GetClientIpAddress();
+    bool IsSuspiciousIp(string ip);
+    Task<string> GetLocationAsync(string ip);
 }
