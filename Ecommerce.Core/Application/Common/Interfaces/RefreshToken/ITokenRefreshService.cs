d4b2e55dbfed86f552038f378a41a4db84e7dffc
slothnetdev <sungshigun@gmail.com>
2025-12-05 13:43:30 +0800

    feat: Implement and test refresh token endpoint and its corresponding service method.

--- Ecommerce.Core/Application/Common/Interfaces/RefreshToken/ITokenRefreshService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/RefreshToken/ITokenRefreshService.cs
@@ -1,6 +1,12 @@
+using Ecommerce.Shared.TokenDTO;
+using Ecommerce.Shared.Wrapper;
+
 namespace Ecommerce.Core.Application.Common.Interfaces.RefreshToken;
 
 public interface ITokenRefreshService
 {
-    
+    Task<ResponseType<TokenResponseDto>> RefreshTokenAsync(
+        string accessToken, 
+        string refreshToken, 
+        string ipAddress);
 }
