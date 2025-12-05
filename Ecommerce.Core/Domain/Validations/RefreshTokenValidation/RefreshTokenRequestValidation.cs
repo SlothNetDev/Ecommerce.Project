c7c0fd6be7d6abe486431d04da75d5b686dec321
slothnetdev <sungshigun@gmail.com>
2025-12-05 16:04:35 +0800

    fix: Update validation messages in RefreshTokenRequestValidation for clarity and consistency

--- Ecommerce.Core/Domain/Validations/RefreshTokenValidation/RefreshTokenRequestValidation.cs
+++ Ecommerce.Core/Domain/Validations/RefreshTokenValidation/RefreshTokenRequestValidation.cs
@@ -1,6 +1,16 @@
+using Ecommerce.Shared.TokenDTO;
+using FluentValidation;
+
 namespace Ecommerce.Core.Domain.Validations.RefreshTokenValidation;
 
-public class RefreshTokenRequestValidation
+public class RefreshTokenRequestValidation : AbstractValidator<RefreshTokenRequestDto>
 {
-
+    public RefreshTokenRequestValidation()
+    {
+        RuleFor(x => x.RefreshToken).NotNull().NotEmpty()
+            .WithMessage("Bearer Token  is required");
+        
+        RuleFor(x => x.RefreshToken).NotNull().NotEmpty()
+            .WithMessage("Refresh Token  is required");
+    }
 }
