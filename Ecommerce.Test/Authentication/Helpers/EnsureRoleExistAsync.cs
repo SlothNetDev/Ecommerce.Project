02ece768cba95e102477c06600353d670424ccec
slothnetdev <sungshigun@gmail.com>
2025-12-08 06:30:17 +0800

    feat: Add RoleExist helper class to ensure required roles are created in the database for authentication tests

---
+++ Ecommerce.Test/Authentication/Helpers/EnsureRoleExistAsync.cs
@@ -0,0 +1,29 @@
+using Ecommerce.Api;
+using Ecommerce.Infrastructure.Identity.Entities;
+using Ecommerce.Test.TestUtilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Test.Authentication.Helpers;
+
+public  class RoleExist : TestBase
+{
+    public RoleExist(CustomWebApplicationFactory<Program> factory) : base(factory)
+    {
+        EnsureRolesExistAsync().Wait();
+    }
+
+    public async Task EnsureRolesExistAsync()
+    {
+        var roleManager = _factory.Services.GetRequiredService<RoleManager<ApplicationRole>>();
+        var roles = new[] { "Admin", "Costumer", "Seller" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new ApplicationRole { Name = role });
+            }
+        }
+    }
+}
