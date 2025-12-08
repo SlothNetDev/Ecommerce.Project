72e1b5a917d9f74d95f0d86245d6b4bd40cf3a2f
slothnetdev <sungshigun@gmail.com>
2025-12-08 08:58:24 +0800

    refactor: Remove unused using directive from OrderUpdateDto to enhance code clarity

--- Ecommerce.Shared/OrdersDto/OrderDto/OrderUpdateDto.cs
+++ Ecommerce.Shared/OrdersDto/OrderDto/OrderUpdateDto.cs
@@ -1,9 +1,9 @@
-﻿using Ecommerce.Shared.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ecommerce.Shared.Enums;
 
 namespace Ecommerce.Shared.OrdersDto.OrderDto
 {
