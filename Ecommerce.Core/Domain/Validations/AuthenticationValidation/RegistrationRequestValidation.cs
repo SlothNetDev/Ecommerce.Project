b611e69b61364beaea0eebbf92c2610e9209b0ef
slothnetdev <sungshigun@gmail.com>
2025-12-08 08:34:02 +0800

    feat: Add RegistrationRequestValidation class to validate user registration requests, ensuring required fields and password complexity rules

---
+++ Ecommerce.Core/Domain/Validations/AuthenticationValidation/RegistrationRequestValidation.cs
@@ -0,0 +1,35 @@
+using Ecommerce.Shared.AuthenticationDTO;
+using FluentValidation;
+
+namespace Ecommerce.Core.Domain.Validations.AuthenticationValidation;
+
+public class RegistrationRequestValidation : AbstractValidator<RegisterRequestDto>
+{
+    public RegistrationRequestValidation()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100).WithMessage("First name must not exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(100).WithMessage("Last name must not exceed 100 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("A valid email address is required")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+            .Matches(@"[\W_]").WithMessage("Password must contain at least one special character");
+        
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty().WithMessage("Please confirm your password")
+            .Equal(x => x.Password).WithMessage("Passwords do not match");
+    }
+}
