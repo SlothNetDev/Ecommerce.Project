b31bf86982eabb650732da7286fed68b687a95ec
slothnetdev <sungshigun@gmail.com>
2025-12-08 08:55:35 +0800

    refactor: Update AuthenticationService to use JwtToken and Register interfaces, improving code organization and clarity

--- Ecommerce.Infrastructure/Identity/Services/AuthenticationService.cs
+++ Ecommerce.Infrastructure/Identity/Services/AuthenticationService.cs
@@ -1,5 +1,6 @@
 using Ecommerce.Core.Application.Common.Interfaces;
-using Ecommerce.Core.Application.Common.Interfaces.RefreshToken;
+using Ecommerce.Core.Application.Common.Interfaces.JwtToken;
+using Ecommerce.Core.Application.Common.Interfaces.Register;
 using Ecommerce.Core.Application.Settings;
 using Ecommerce.Infrastructure.Data;
 using Ecommerce.Infrastructure.Identity.Entities;
