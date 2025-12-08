c9dc710eb07596f2198925e8e4c7f08a3811887a
slothnetdev <sungshigun@gmail.com>
2025-12-09 06:15:02 +0800

    feat: Enhance AuthJobs class to include SendOtpJob method for sending OTP emails

--- Ecommerce.Infrastructure/BackgroundJobs/AuthJobs.cs
+++ Ecommerce.Infrastructure/BackgroundJobs/AuthJobs.cs
@@ -1,6 +1,11 @@
+using Ecommerce.Core.Application.Common.Interfaces.Register;
+
 namespace Ecommerce.Infrastructure.BackgroundJobs;
 
-public class AuthJobs
+public class AuthJobs(IEmailService emailService)
 {
-
+    public async Task SendOtpJob(string email, string code, string name)
+    {
+        await emailService.SendOtpEmailAsync(email, code, name);
+    }
 }
