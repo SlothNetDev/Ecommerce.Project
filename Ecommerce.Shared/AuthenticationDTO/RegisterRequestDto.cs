031523cd1c3fa20694ddb15a190576afd845ad23
slothnetdev <sungshigun@gmail.com>
2025-12-08 08:13:14 +0800

    feat: Enhance RegisterRequestDto by adding FirstName, LastName, and ConfirmPassword fields for improved user registration details

--- Ecommerce.Shared/AuthenticationDTO/RegisterRequestDto.cs
+++ Ecommerce.Shared/AuthenticationDTO/RegisterRequestDto.cs
@@ -5,7 +5,10 @@ namespace Ecommerce.Shared.AuthenticationDTO;
 
 public record RegisterRequestDto()
 {
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
     public string Email { get; init; } = string.Empty;
     public string Password { get; init; } = string.Empty;
-    public string UserName { get; init; } =  string.Empty;
+    public string ConfirmPassword { get; init; } = string.Empty;
+    
 };
