33cb1bb1f37eabd1a84cdce9196d225a250461e3
slothnetdev <sungshigun@gmail.com>
2025-12-10 10:13:45 +0800

    refactor: Update ISellerApplicationService to use SellerApplicationResponse for application submission and retrieval

--- Ecommerce.Core/Application/Common/Interfaces/ISellerApplicationService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/ISellerApplicationService.cs
@@ -1,11 +1,13 @@
+using Ecommerce.Shared;
+using Ecommerce.Shared.SellerApplication;
 using Ecommerce.Shared.Wrapper;
 
 namespace Ecommerce.Core.Application.Common.Interfaces;
 
 public interface ISellerApplicationService
 {
-    Task<ResponseType<SellerApplicationDto>> SubmitApplication(SubmitApplicationRequest request);
-    Task<ResponseType<List<SellerApplicationDto>>> GetPendingApplications();
+    Task<ResponseType<SellerApplicationResponse>> SubmitApplication(SubmitApplicationRequest request);
+    Task<ResponseType<List<SellerApplicationResponse>>> GetPendingApplications();
     Task<ResponseType<bool>> ApproveApplication(Guid applicationId, string adminComments, Guid adminId);
     Task<ResponseType<bool>> RejectApplication(Guid applicationId, string adminComments, Guid adminId);
 }
