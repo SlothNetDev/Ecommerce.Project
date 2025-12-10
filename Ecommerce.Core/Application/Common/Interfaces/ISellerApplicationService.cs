ab10c14c1fa687e5487c2c58e30d45b8ca2e9fe4
slothnetdev <sungshigun@gmail.com>
2025-12-10 10:04:46 +0800

    refactor: Update ISellerApplicationService interface to include methods for submitting, approving, and rejecting seller applications

--- Ecommerce.Core/Application/Common/Interfaces/ISellerApplicationService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/ISellerApplicationService.cs
@@ -1,6 +1,11 @@
+using Ecommerce.Shared.Wrapper;
+
 namespace Ecommerce.Core.Application.Common.Interfaces;
 
-public class ISellerApplicationService
+public interface ISellerApplicationService
 {
-
+    Task<ResponseType<SellerApplicationDto>> SubmitApplication(SubmitApplicationRequest request);
+    Task<ResponseType<List<SellerApplicationDto>>> GetPendingApplications();
+    Task<ResponseType<bool>> ApproveApplication(Guid applicationId, string adminComments, Guid adminId);
+    Task<ResponseType<bool>> RejectApplication(Guid applicationId, string adminComments, Guid adminId);
 }
