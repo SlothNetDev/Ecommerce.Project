ceffd772b21933b9034ea1ad9dea0e215dfe044a
slothnetdev <sungshigun@gmail.com>
2025-12-10 11:15:11 +0800

    feat: Enhance IRoleManagementService with methods for changing and retrieving user roles

--- Ecommerce.Core/Application/Common/Interfaces/IRoleManagerService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/IRoleManagerService.cs
@@ -1,6 +1,9 @@
+using Ecommerce.Shared.Wrapper;
+
 namespace Ecommerce.Core.Application.Common.Interfaces;
 
-public interface IRoleManagerService
+public interface IRoleManagementService  
 {
-    
+    Task<ResponseType<bool>> ChangeUserRole(Guid userId, string newRole, Guid changedBy, string reason);
+    Task<ResponseType<List<string>>> GetUserRoles(Guid userId);
 }
