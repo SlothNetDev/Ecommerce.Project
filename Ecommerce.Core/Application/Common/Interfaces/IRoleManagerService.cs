f24ec28344cdd9d4fb31caad2843b2062ac387b3
ProgrammerNoob <sungshigun@gmail.com>
2025-12-13 11:22:10 +0800

    feat: Add methods to manage user roles in IRoleManagementService interface

--- Ecommerce.Core/Application/Common/Interfaces/IRoleManagerService.cs
+++ Ecommerce.Core/Application/Common/Interfaces/IRoleManagerService.cs
@@ -4,6 +4,20 @@ namespace Ecommerce.Core.Application.Common.Interfaces;
 
 public interface IRoleManagementService  
 {
+    /// <summary>
+    /// Crete a way to change the User Role
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="newRole"></param>
+    /// <param name="changedBy"></param>
+    /// <param name="reason"></param>
+    /// <returns></returns>
     Task<ResponseType<bool>> ChangeUserRole(Guid userId, string newRole, Guid changedBy, string reason);
+    
+    /// <summary>
+    /// Get the User Roles 
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
     Task<ResponseType<List<string>>> GetUserRoles(Guid userId);
 }
