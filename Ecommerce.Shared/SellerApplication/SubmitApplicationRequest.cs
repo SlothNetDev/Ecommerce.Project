0a3e4838500215502984aa1fab077b878e126f11
ProgrammerNoob <sungshigun@gmail.com>
2025-12-15 09:46:46 +0800

    feat: Adding the sellerId in SubmitApplicationRequest.cs dto

--- Ecommerce.Shared/SellerApplication/SubmitApplicationRequest.cs
+++ Ecommerce.Shared/SellerApplication/SubmitApplicationRequest.cs
@@ -2,6 +2,8 @@ namespace Ecommerce.Shared.SellerApplication;
 
 public record SubmitApplicationRequest()
 {
+    public Guid SellerId { get; init; }
+    
     public string BusinessName { get; init; } = string.Empty;
  
     public string ApplicationReason{get;init;} = string.Empty;
