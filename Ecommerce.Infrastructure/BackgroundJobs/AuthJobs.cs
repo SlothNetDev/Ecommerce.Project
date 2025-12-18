d622e6a2ad094c3b7961d7362650a68e1c1631c2
ProgrammerNoob <sungshigun@gmail.com>
2025-12-18 08:08:16 +0800

    feat: Introduce `IBackgroundJobService` with Hangfire and fake implementations

--- Ecommerce.Infrastructure/BackgroundJobs/AuthJobs.cs
+++ Ecommerce.Infrastructure/BackgroundJobs/AuthJobs.cs
@@ -1,5 +1,7 @@
+using System.Linq.Expressions;
 using Ecommerce.Core.Application.Common.Interfaces.Notification;
 using Ecommerce.Core.Application.Common.Interfaces.Register;
+using Hangfire;
 
 namespace Ecommerce.Infrastructure.BackgroundJobs;
 
@@ -8,5 +10,24 @@ public class AuthJobs(IEmailService emailService)
     public async Task SendOtpJob(string email, string code, string name)
     {
         await emailService.SendOtpEmailAsync(email, code, name);
+    }
+}
+
+public interface IBackgroundJobService
+{
+    void Enqueue(Expression<Action> job);
+}
+public class HangfireBackgroundJobService : IBackgroundJobService
+{
+    public void Enqueue(Expression<Action> job)
+        => BackgroundJob.Enqueue(job);
+}
+public class FakeBackgroundJobService : IBackgroundJobService
+{
+    public List<string> EnqueuedJobs { get; } = new();
+
+    public void Enqueue(Expression<Action> job)
+    {
+        EnqueuedJobs.Add(job.Body.ToString());
     }
 }
