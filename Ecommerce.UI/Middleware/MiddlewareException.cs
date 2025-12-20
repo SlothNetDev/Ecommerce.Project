46f1e3ddd72319feac01959f867d661719fc88b1
ProgrammerNoob <sungshigun@gmail.com>
2025-12-20 21:21:33 +0800

    refactor: Update `MiddlewareException` namespace for consistency with `Ecommerce.UI` structure

--- Ecommerce.UI/Middleware/MiddlewareException.cs
+++ Ecommerce.UI/Middleware/MiddlewareException.cs
@@ -1,7 +1,7 @@
 using System.Net;
 using System.Text.Json;
 
-namespace Ecommerce.Api.Middleware;
+namespace Ecommerce.UI.Middleware;
 
 public class MiddlewareException(RequestDelegate next, ILogger<MiddlewareException> logger)
 {
