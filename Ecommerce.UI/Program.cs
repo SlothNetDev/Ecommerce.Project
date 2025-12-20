72c2c471c7221a3ad0103b9b7c940554c484f130
ProgrammerNoob <sungshigun@gmail.com>
2025-12-20 21:20:51 +0800

    refactor: Simplify `Program.cs` by using extension methods for service configuration and application setup

--- Ecommerce.UI/Program.cs
+++ Ecommerce.UI/Program.cs
@@ -1,27 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ecommerce.UI.Extensions;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+namespace Ecommerce.UI;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.AddCleanSerilog()
+            .Services
+            .AddPresentationService(builder.Configuration);
+
+        var app = builder.Build();
+
+        await app.ConfigureApplication();
+
+        await app.RunAsync();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
