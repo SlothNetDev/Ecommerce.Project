59350606f7cdf3078b7f2653f2558169fc76a6c4
ProgrammerNoob <sungshigun@gmail.com>
2025-12-20 21:17:51 +0800

    refactor: Update `ApplicationBuilderExtensions` for `Ecommerce.UI` to configure MVC, static files, and error handling

--- Ecommerce.UI/Extensions/ApplicationBuilderExtensions.cs
+++ Ecommerce.UI/Extensions/ApplicationBuilderExtensions.cs
@@ -1,27 +1,29 @@
 ﻿using Ecommerce.Api.Middleware;
 using Hangfire;
 
-namespace Ecommerce.Api.Extensions;
+namespace Ecommerce.UI.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
     public static async Task<WebApplication> ConfigureApplication(this WebApplication app)
     {
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Testing"))
+        if (!app.Environment.IsDevelopment() && !app.Environment.IsEnvironment("Testing"))
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Ecommerce v1");
-                options.RoutePrefix = "api-docs"; // Optional: Changes the URL from /swagger to /api-docs
-            });
-        }
-
-        if (!app.Environment.IsEnvironment("Testing"))
-        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
             app.UseHangfireDashboard();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
         //adding global middleware
         app.UseMiddleware<MiddlewareException>();
