f42887910cc06bc39f73e2f05ea1a8644a419345
ProgrammerNoob <sungshigun@gmail.com>
2025-12-21 09:01:54 +0800

    refactor: Fix typo in `RoleSeeder` and make class static

--- Ecommerce.Infrastructure/Data/Seeders/RoleSeeder.cs
+++ Ecommerce.Infrastructure/Data/Seeders/RoleSeeder.cs
@@ -1,9 +1,9 @@
 namespace Ecommerce.Infrastructure.Data.Seeders;
 
 //Roles available 
-public class RoleSeeder
+public static class RoleSeeder
 {
     public const string Admin = "Admin";
-    public const string Costumer = "Costumer";
+    public const string Customer = "Customer";
     public const string Seller = "Seller";
 }
