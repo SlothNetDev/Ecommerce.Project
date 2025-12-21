b0262940f39cb3c21eaf784376cba551a92fa51b
ProgrammerNoob <sungshigun@gmail.com>
2025-12-22 04:08:26 +0800

    feat: add FailureType enum for HTTP status codes

---
+++ Ecommerce.Shared/Enums/FailureType.cs
@@ -0,0 +1,17 @@
+﻿namespace Ecommerce.Shared.Enums;
+
+/// <summary>
+/// Possible HTTP STATUS CODES That likely failed 
+/// </summary>
+public enum FailureType
+{
+    Validation,
+    Authentication,
+    Authorization,
+    NotFound,
+    Conflict,
+    RateLimited,
+    Expired,
+    Locked,
+    Internal
+}
