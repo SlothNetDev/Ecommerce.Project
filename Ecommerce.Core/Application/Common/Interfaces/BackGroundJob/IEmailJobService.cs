02cb03fa8e08db60982720e69f3ffc3be5cce488
ProgrammerNoob <sungshigun@gmail.com>
2025-12-24 07:43:51 +0800

    feat: Adding IEmailJobService.cs for email job handling and service interface

---
+++ Ecommerce.Core/Application/Common/Interfaces/BackGroundJob/IEmailJobService.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Core.Application.Common.Interfaces.BackGroundJob;
+
+public interface IEmailJobService
+{
+    Task SendOtpAsync(string email, string otp, string? name);
+    Task SendWelcomeAsync(string email, string userName, string firstName);
+    Task SendPasswordResetAsync(string email, string resetToken, string userName);
+}
