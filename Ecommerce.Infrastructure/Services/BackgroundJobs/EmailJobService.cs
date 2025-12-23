2e9fddc72564919963413e7c4d764b16046b2c6b
ProgrammerNoob <sungshigun@gmail.com>
2025-12-24 07:42:47 +0800

    feat: Adding EmailJobService to handle Hangfire email jobs and we can safely switch email provider service

---
+++ Ecommerce.Infrastructure/Services/BackgroundJobs/EmailJobService.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Core.Application.Common.Interfaces.BackGroundJob;
+using Ecommerce.Core.Application.Common.Interfaces.Notification;
+
+namespace Ecommerce.Infrastructure.Services.BackgroundJobs;
+
+/// <summary>
+/// Will be used by Hangfire to send emails. By this we can safely swap Email Providers
+/// </summary>
+/// <param name="emailService"></param>
+public class EmailJobService(IEmailService emailService) : IEmailJobService
+{
+    public Task SendOtpAsync(string email, string otp, string? name)
+        => emailService.SendOtpEmailAsync(email, otp, name);
+
+    public Task SendWelcomeAsync(string email, string userName, string firstName)
+     => emailService.SendWelcomeEmailAsync(email, userName, firstName);
+
+    public Task SendPasswordResetAsync(string email, string resetToken, string userName)
+    => emailService.SendPasswordResetEmailAsync(email, resetToken, userName);
+}
