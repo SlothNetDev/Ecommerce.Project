40360c3b4ca4efefc124ef87004536ce1102937f
ProgrammerNoob <sungshigun@gmail.com>
2025-12-24 19:45:25 +0800

    feat: Add EmailOtpConfiguration for configuring EmailOtp entity in EF Core

---
+++ Ecommerce.Infrastructure/Data/Configuration/Authentication/EmailOtpConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Infrastructure.Identity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Authentication;
+
+public class EmailOtpConfiguration : IEntityTypeConfiguration<EmailOtp>
+{
+    public void Configure(EntityTypeBuilder<EmailOtp> builder)
+    {
+        builder.ToTable("Email_Otp");
+
+        builder.HasKey(x => x.EmailOtpId);
+
+        builder.Property(x => x.CodeHash)
+            .HasMaxLength(256)
+            .IsRequired();
+        
+        builder.Property(x => x.Attempts)
+            .IsRequired()
+            .HasDefaultValue(0);
+        
+        //relationship
+        builder.HasOne(x => x.ApplicationUser)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        //Index
+        builder.HasIndex(x => new {x.UserId, x.VerifiedAt});
+
+        builder.HasIndex(x => x.ExpiresAt);
+        
+        builder.HasIndex(x => x.CreatedAt);
+    }
+}
