b57c799d228ee87f0c1c8c560d9ff17e882e3558
ProgrammerNoob <sungshigun@gmail.com>
2025-12-24 19:45:56 +0800

    feat: Add EmailOtp entity and related metadata for user authentication OTPs

---
+++ Ecommerce.Infrastructure/Identity/Entities/EmailOtp.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Infrastructure.Identity.Entities;
+
+/// <summary>
+/// Represents a one-time password (OTP) sent through email for user authentication purposes.
+/// This entity stores the OTP details along with metadata regarding its expiration, verification, and usage attempts.
+/// </summary>
+public class EmailOtp
+{
+    public Guid EmailOtpId { get; set; } = Guid.NewGuid();
+   
+    //relationship with user
+    public Guid UserId { get; set; }
+    public ApplicationUser ApplicationUser { get; set; } = null!;
+    
+    //OtpData
+    public string CodeHash { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public DateTime? VerifiedAt { get; set; }
+    
+    //Meta Data
+    public int Attempts { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public bool IsExpired => DateTime.UtcNow >= ExpiresAt; //expression embodied property
+}
