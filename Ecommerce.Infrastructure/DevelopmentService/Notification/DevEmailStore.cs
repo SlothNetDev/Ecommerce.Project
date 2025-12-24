193f8d1c0ff0f843a550a6fb8e9eec9edf566def
ProgrammerNoob <sungshigun@gmail.com>
2025-12-24 18:52:57 +0800

    feat: Add DevEmailStore and configure email service for development environment

---
+++ Ecommerce.Infrastructure/DevelopmentService/Notification/DevEmailStore.cs
@@ -0,0 +1,21 @@
+﻿namespace Ecommerce.Infrastructure.DevelopmentService.Notification;
+
+public class DevEmailStore
+{
+    private readonly List<DevEmailMessage> _devEmailMessages = new();
+
+    public void Add(DevEmailMessage emailMessage) => _devEmailMessages.Add(emailMessage);
+    
+    public List<DevEmailMessage> GetAllEmails()
+    {
+        return _devEmailMessages.OrderByDescending(x => x.SentAt).ToList();
+    }
+}
+
+
+public record DevEmailMessage(
+    string To,
+    string Subject,
+    string Body,
+    DateTime SentAt
+);
