8b12b3a347541b2b1fe4853d248a040cfda13501
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 17:24:59 +0800

    feat: Add IHashService interface for hashing and verifying passwords, this also uses for OTP  hashing

---
+++ Ecommerce.Core/Application/Common/Interfaces/Security/IHashService.cs
@@ -0,0 +1,7 @@
+﻿namespace Ecommerce.Core.Application.Common.Interfaces.Security;
+
+public interface IHashService
+{
+    string Hash(string input);
+    bool Verify(string input, string hashedInput);
+}
