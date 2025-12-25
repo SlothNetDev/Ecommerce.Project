672047884397588cfe7e2cf9f1aff516c282272d
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 21:48:42 +0800

    feat: Add security key validation for OTP and update

--- Ecommerce.Core/Application/Settings/JwtSettings.cs
+++ Ecommerce.Core/Application/Settings/JwtSettings.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Ecommerce.Core.Application.Settings;
 
 public class JwtSettings
 {
+    [Required(ErrorMessage = "Jwt Key is required.")]
     public string Key { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
