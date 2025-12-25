45352457acd30facec95a16cb5dee96354c6ecd6
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 17:25:41 +0800

    feat: Update EmailVerificationRequestDto to use UserId instead of 6-digit OTP verification code

--- Ecommerce.Shared/RegisterDto/EmailVerificationRequestDto.cs
+++ Ecommerce.Shared/RegisterDto/EmailVerificationRequestDto.cs
@@ -6,13 +6,7 @@ namespace Ecommerce.Shared.RegisterDto;
 /// </summary>
 public record EmailVerificationRequestDto()
 {
-    /// <summary>
-    /// The email address to verify.
-    /// </summary>
+    public Guid UserId  { get; init; }
     public string Email { get; init; } = string.Empty;
-
-    /// <summary>
-    /// The 6-digit OTP verification code sent to the email address.
-    /// </summary>
     public string VerificationCode { get; init; } = string.Empty;
 };
