6cc98aaf4b0076fb0da6699d039f2b6fb5fc4f16
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 17:23:55 +0800

    feat: Add EmailOtp entity and related metadata for user authentication OTPs

---
+++ Ecommerce.Infrastructure/Migrations/20251225014438_Add Anti abuse and Resend.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAntiabuseandResend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Email_Otp",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastSentAt",
+                table: "Email_Otp",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResentCount",
+                table: "Email_Otp",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastSentAt",
+                table: "Email_Otp");
+
+            migrationBuilder.DropColumn(
+                name: "ResentCount",
+                table: "Email_Otp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Email_Otp",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
