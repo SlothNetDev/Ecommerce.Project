3c84a117d09a5ec5eb285525b64b36690884e847
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 21:49:15 +0800

    configure: Add SecurityKeySettings and update constructor in HmacHashService

---
+++ Ecommerce.Core/Application/Settings/SecurityKeySettings.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Core.Application.Settings;
+
+public class SecurityKeySettings
+{
+    [Required(ErrorMessage = "Security Key is required.")]
+    public string SecurityKey { get; set; } = string.Empty;
+}
