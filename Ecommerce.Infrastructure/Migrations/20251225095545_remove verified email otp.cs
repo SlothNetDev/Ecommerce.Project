236266a660dca6fdca6c5d657720857d6b7ec557
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 18:33:29 +0800

    chore: Remove verifiedAt column from Email_Otp table and add unique index for verification

---
+++ Ecommerce.Infrastructure/Migrations/20251225095545_remove verified email otp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeverifiedemailotp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Email_Otp",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Email_Otp_UserId",
+                table: "Email_Otp",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Email_Otp_UserId",
+                table: "Email_Otp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Email_Otp",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
