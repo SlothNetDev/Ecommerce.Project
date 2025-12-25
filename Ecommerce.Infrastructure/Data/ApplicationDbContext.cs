3808d6a6ef74d26c790e4d73628fc71f3740c74b
ProgrammerNoob <sungshigun@gmail.com>
2025-12-25 17:26:11 +0800

    feat: Add EmailOtpDb and EmailOtpConfiguration to ApplicationDbContext

--- Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
+++ Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -17,27 +17,37 @@ namespace Ecommerce.Infrastructure.Data
         public virtual DbSet<ApplicationUser> ApplicationUsersDb { get; set; } = null!;
         public virtual DbSet<ApplicationToken> RefreshToken { get; set; }
         
+        public virtual DbSet<EmailOtp> EmailOtpDb { get; set; } = null!;
+        
         public virtual DbSet<Category> CategoriesDb { get; set; } = null!;
         public virtual DbSet<Product> ProductsDb { get; set; } = null!;
         public virtual DbSet<Order> OrderDb { get; set; } = null!;
         public virtual DbSet<OrderItem> OrderItemDb { get; set; } = null!;
         public virtual DbSet<Review> ReviewDb { get; set; } = null!;
-        
-        
         public virtual DbSet<SellerApplication> SellerApplicationDb { get; set; } = null!;
-
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
+
+            #region Authentication
+            builder.ApplyConfigurationsFromAssembly(typeof(EmailOtpConfiguration).Assembly);
             builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
             builder.ApplyConfigurationsFromAssembly(typeof(ApplicationTokenConfiguration).Assembly);
-
+            builder.ApplyConfigurationsFromAssembly(typeof(RoleHistoryConfiguration).Assembly);
+            #endregion
+           
+            //Catalog
             builder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
             builder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+            
+            //Orders
             builder.ApplyConfigurationsFromAssembly(typeof(OrderItemConfiguration).Assembly);
             builder.ApplyConfigurationsFromAssembly(typeof(OrderConfiguration).Assembly);
+            
+            //Reviews
             builder.ApplyConfigurationsFromAssembly(typeof(ReviewConfiguration).Assembly);
+            
         }
     }
 }
