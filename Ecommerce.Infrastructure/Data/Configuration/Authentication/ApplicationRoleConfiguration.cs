27293dda7581a4be6c81adb8e9e94775096c81de
jervi <sungshigun@gmail.com>
2026-01-04 21:09:37 +0800

    refactor: update data source configuration and add SQLite path utilities

---
+++ Ecommerce.Infrastructure/Data/Configuration/Authentication/ApplicationRoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Infrastructure.Identity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Data.Configuration.Authentication;
+
+public class ApplicationRoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
+{
+    public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+    {
+        builder.ToTable("ApplicationRoles");
+        builder.Property(x => x.Description)
+            .IsRequired().HasMaxLength(128);
+        
+    }
+}
