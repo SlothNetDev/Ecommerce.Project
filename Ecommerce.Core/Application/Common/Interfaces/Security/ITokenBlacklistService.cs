5ed621bb4dcb278c432a0a6095074a3c92dbdc4a
jervi <sungshigun@gmail.com>
2026-01-06 11:17:45 +0800

    feat: add ITokenBlacklistService interface for token management

---
+++ Ecommerce.Core/Application/Common/Interfaces/Security/ITokenBlacklistService.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Core.Application.Common.Interfaces.Security;
+
+public interface ITokenBlacklistService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="jti"></param>
+    /// <param name="expiresIn"></param>
+    /// <returns></returns>
+    Task BlackListTokenAsync(string jti, TimeSpan expiresIn);
+    Task<bool> IsTokenBlacklistedAsync(string jti);
+}
