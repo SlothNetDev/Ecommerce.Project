e0df73c6f4d31bec41f0cd63d886c3f43ff2ada5
jervi <sungshigun@gmail.com>
2026-01-09 11:05:51 +0800

    refactor: update HmacHashService to use IOptions for SecurityKeySettings

--- Ecommerce.Infrastructure/Security/HmacHashService.cs
+++ Ecommerce.Infrastructure/Security/HmacHashService.cs
@@ -2,18 +2,20 @@
 using System.Text;
 using Microsoft.Extensions.Configuration;
 using Ecommerce.Core.Application.Common.Interfaces.Security;
+using Ecommerce.Core.Application.Settings;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Ecommerce.Infrastructure.Security;
 
 public class HmacHashService : IHashService
 {
     private readonly byte[] _key;
-    public HmacHashService(IConfiguration configuration, ILogger<HmacHashService> logger)
+    public HmacHashService(IOptions<SecurityKeySettings> configuration, ILogger<HmacHashService> logger)
     {
-        var secret = configuration["Security:Key"];
+        var secret = configuration;
         
-        _key = Encoding.UTF8.GetBytes(secret);
+        _key = Encoding.UTF8.GetBytes(secret.Value.SecurityKey);
     }
     public string Hash(string input)
     {
